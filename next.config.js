25e4c8d1ed5399c5a8efd4df9262e324a83775b1
BhattShriya <shriyabhatt9407@gmail.com>
2025-07-22 16:39:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/WDT-Project',
+  assetPrefix: '/WDT-Project/',
   images: {
     unoptimized: true,
     domains: ["localhost"],
